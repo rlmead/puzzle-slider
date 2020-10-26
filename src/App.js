cc7f7f3781736b233fe5b2bdcc8c59cbfd681f6e
Rebecca Mead <beckyleemead@gmail.com>
2020-10-26 12:11:19 -0400

    function to choose a valid move given a tile index

--- src/App.js
+++ src/App.js
@@ -9,32 +9,15 @@ class App extends React.Component {
     this.state = {
       // keep track of image order
       // for rendering images and checking solution
-      puzzle: [
-        {display: 0, solution: 0},
-        {display: 1, solution: 1},
-        {display: 2, solution: 2},
-        {display: 3, solution: 3},
-        {display: 4, solution: 4},
-        {display: 5, solution: 5},
-        {display: 6, solution: 6},
-        {display: 7, solution: 7},
-        {display: 8, solution: 8},
-        {display: 9, solution: 9},
-        {display: 10, solution: 10},
-        {display: 11, solution: 11},
-        {display: 12, solution: 12},
-        {display: 13, solution: 13},
-        {display: 14, solution: 14},
-        {display: 15, solution: 15},
-      ],
+      puzzle: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
       solved: false,
     };
-    this.addImage=this.addImage.bind(this);
-    this.shufflePuzzle=this.shufflePuzzle.bind(this);
-    this.moveTile=this.moveTile.bind(this);
-    this.checkSolved=this.checkSolved.bind(this);
+    this.addImage = this.addImage.bind(this);
+    this.shufflePuzzle = this.shufflePuzzle.bind(this);
+    this.swapTiles = this.swapTiles.bind(this);
+    this.checkSolved = this.checkSolved.bind(this);
   }
-
+  
   // componentDidMount: load state from localStorage
   componentDidMount() {
     let puzzleState = JSON.parse(window.localStorage.getItem('puzzleState'));
@@ -59,19 +42,32 @@ class App extends React.Component {
     // redisplay top right corner (index 3) with all-black square
   }
 
+  // function to swap two tiles
+  swapTiles(tile1, tile2) {
+    console.log(`swapping tiles ${tile1} & ${tile2}`);
+  }
+
+  // function to choose a random valid move
+  chooseMove(tileIndex) {
+    let validMoves = [-1, 1, -4, 4];
+    let nextMove = tileIndex + validMoves[Math.floor(Math.random() * 4)];
+    while (nextMove > 15 || nextMove < 0){
+      nextMove = tileIndex + validMoves[Math.floor(Math.random() * 4)];
+    }
+    return nextMove;
+  }
+
   // function to shuffle puzzle
   shufflePuzzle() {
     // choose a random tile to be the blank tile
-    let blankTile=Math.floor(Math.random() * 16);
-    
+    let blankTile = Math.floor(Math.random() * 16);
+    // run moveTile on blankTile in random directions 10x
+    for (let i = 0; i < 10; i++) {
+      let nextMove = this.chooseMove(blankTile);
+      this.swapTiles(blankTile,nextMove);
+    }
   }
 
-  // function to move a tile
-  // to the adjacent empty square
-  // when tile is clicked
-  moveTile(tileIndex) {
-    console.log('moving tile');
-  }
 
   // function to check if the puzzle has been solved
   // to be run anytime a tile moves (componentDidUpdate?)
@@ -85,17 +81,17 @@ class App extends React.Component {
         <h1 className="mb-4">solve this puzzle!</h1>
         {/* image input: drag & drop or choose file */}
         {/* when an image has been selected: */}
-          {/* render sliced-up image with cutout */}
-          {/* render button: shuffle puzzle */}
-        <Board 
+        {/* render sliced-up image with cutout */}
+        {/* render button: shuffle puzzle */}
+        <Board
           puzzleState={this.state.puzzle}
         />
         <button
-        type="button"
-        className="btn btn-primary"
-        onClick={this.shufflePuzzle}
+          type="button"
+          className="btn btn-primary"
+          onClick={this.shufflePuzzle}
         >
-        shuffle
+          shuffle
         </button>
       </div>
     );
