24db8d9486504a4e31492cb06e2ffec1ddd43dd5
Rebecca Mead <beckyleemead@gmail.com>
2020-10-27 11:33:05 -0400

    refactor grid with for loops and reactstrap

--- src/Board.js
+++ src/Board.js
@@ -1,49 +1,50 @@
 import React from 'react';
 import DefaultPic from './victoria-street.jpg';
+import { Container, Row, Col } from 'reactstrap';
 
 function Board(props) {
-  let content = [];
-  for (let i = 0; i < props.puzzleState.length; i++) {
-    let item = props.puzzleState[i];
-    content.push(
-      <div
-        className='col-3'
-        key={i}
-        onClick={() => props.moveTile(i)}
-        style={{
-          maxWidth: '150px',
-          minWidth: '150px',
-          maxHeight: '150px',
-          minHeight: '150px',
-          overflow: 'hidden',
-          margin: '0',
-          padding: '0',
-        }}>
-        {
-          (!item.blankTile)
-          && <img
-            src={DefaultPic}
+  let grid = [];
+  for (let r = 0; r < 4; r++) {
+    let rowArray = [];
+    for (let c = 0; c < 4; c++) {
+      let item = props.puzzleState[r*4+c];
+      rowArray.push(
+        <Col
+          key={r*4+c}
+          onClick={() => props.moveTile(r*4+c)}
+          style={{
+            maxWidth: '150px',
+            minWidth: '150px',
+            maxHeight: '150px',
+            minHeight: '150px',
+            overflow: 'hidden',
+            margin: '0',
+            padding: '0',
+          }}>
+          {
+            (!item.blankTile)
+            && <img
+              src={DefaultPic}
             // style={{
             //   margin: item.margin
             // }}
             >
-          </img>
-        }
-      </div>
-    );
+            </img>
+          }
+        </Col>
+      );
+    };
+    grid.push(
+      <Row>{rowArray}</Row>
+    )
   };
+
   return (
-    <div className='container text-center mb-4'>
-      <div className='row'>
-        <div className='col-8 offset-2'>
-          <div className='row'>
-            {
-              content
-            }
-          </div>
-        </div>
-      </div>
-    </div>
+    <Container className='text-center mb-4'>
+      {
+        grid
+      }
+    </Container>
   )
 }
 
